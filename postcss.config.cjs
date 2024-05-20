0de0016bfce2175dfa1eb7c0f6950afe7924f78e
Alwin-Sajan <alwinsajan225@gmail.com>
2024-05-20 17:39:25 +0530

    frame full

---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+// postcss.config.js
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    // Add other PostCSS plugins if needed
+  ]
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+export const content = [
+  './index.html', // Include your HTML file
+  './src/**/*.{js,jsx,ts,tsx}', // Include JavaScript and TypeScript files
+  './src/**/*.css' // Include CSS files
+];
+export const theme = {
+  extend: {}, // Optionally extend Tailwind CSS theme
+};
+export const plugins = [];
