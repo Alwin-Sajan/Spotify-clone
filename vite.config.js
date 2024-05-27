5b117dd8e38c95eaa9c71dd26496d8d3215ee11c
Alwin-Sajan <alwinsajan225@gmail.com>
2024-05-27 12:06:57 +0530

    stage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://alwin-sajan.github.io/Spotify-clone',
   css: {
     postcss: './postcss.config.js', // Ensure this points to your PostCSS config
   },
